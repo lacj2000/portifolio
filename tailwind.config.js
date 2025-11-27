66dc7437c260e0199b45c4bf9c91f36f31642dfe
lacj2000 <luizjuniordeveloper@gmail.com>
2025-11-27 20:20:55 -0300

    Update dependencies and enhance Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './public/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+        serif: ['var(--font-playfair)'],
+      },
+      colors: {
+        background: "#050505", 
+        
+        surface: "#0f172a", // Slate-900
+        
+        primary: {
+          DEFAULT: "#3b82f6", // Blue-500 (Normal)
+          hover: "#2563eb",   // Blue-600 (Ao passar o mouse)
+        },
+        
+        txt: {
+          main: "#f8fafc",    // Branco gelo (Slate-50)
+          muted: "#94a3b8",   // Cinza para descrições
+        }
+      },
+    },
   },
   plugins: [],
 }
